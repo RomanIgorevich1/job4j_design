0d8cc6820f9f462e65eccb262dfdbea173fe0d5c
votmo <votmoyapochta1@bk.ru>
2022-10-26 11:37:33 +0300

    Очередь на двух стеках [#160 #335495]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -12,33 +12,19 @@ public class SimpleQueue<T> {
         if (sizeIn == 0 && sizeOut == 0) {
             throw new NoSuchElementException();
         }
-        if (sizeIn > 1) {
-            out.push(in.pop());
-            sizeOut++;
-            sizeIn--;
+        if (sizeIn > sizeOut) {
+            while (sizeIn != 0) {
+                out.push(in.pop());
+                sizeIn--;
+                sizeOut++;
+            }
         }
-        sizeIn--;
-        return in.pop();
+        sizeOut--;
+        return out.pop();
     }
 
     public void push(T value) {
-        if (sizeOut > sizeIn) {
-            while (sizeOut != 0) {
-                in.push(out.pop());
-                sizeIn++;
-                sizeOut--;
-            }
-        }
-        if (sizeIn > 1) {
-            while (sizeIn != 1) {
-                out.push(in.pop());
-                sizeOut++;
-                sizeIn--;
-            }
-        }
-        if (sizeIn >= sizeOut) {
-            in.push(value);
-            sizeIn++;
-        }
+        in.push(value);
+        sizeIn++;
     }
 }
