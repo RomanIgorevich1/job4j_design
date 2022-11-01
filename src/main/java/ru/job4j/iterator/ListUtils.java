d50d4437f23f27919ac8e5291a54f9c6b206d809
votmo <votmoyapochta1@bk.ru>
2022-11-01 11:04:08 +0300

    ListIterator [#350217 #356580]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -25,36 +25,31 @@ public class ListUtils {
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> iterator = list.listIterator();
-        T value = iterator.next();
         while (iterator.hasNext()) {
+            T value = iterator.next();
             if (filter.test(value)) {
                 iterator.remove();
             }
-            value = iterator.next();
         }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> iterator = list.listIterator();
-        T oldValue = iterator.next();
-        do {
+        while (iterator.hasNext()) {
+            T oldValue = iterator.next();
             if (filter.test(oldValue)) {
                 iterator.set(value);
             }
-            oldValue = iterator.next();
         }
-        while (iterator.hasNext());
-
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
         ListIterator<T> iterator = list.listIterator();
-        T value = iterator.next();
         while (iterator.hasNext()) {
+            T value = iterator.next();
             if (elements.contains(value)) {
                 iterator.remove();
             }
-            value = iterator.next();
         }
     }
 }
