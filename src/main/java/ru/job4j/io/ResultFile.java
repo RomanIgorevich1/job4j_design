e0a6407cab093edd45a468574c70f5e80020f0f5
votmo <votmoyapochta1@bk.ru>
2022-12-02 21:55:06 +0300

    FileOutputStream. [#252488 #366768]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,22 +1,27 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
+import java.io.IOException;
 
 public class ResultFile {
     public static void main(String[] args) {
-        int[][] array = new int[2][2];
+        StringBuilder matrix = new StringBuilder();
+        int[][] array = new int[4][4];
         for (int i = 0; i < array.length; i++) {
             for (int y = 0; y < array.length; y++) {
                 array[i][y] = (i + 1) * (y + 1);
+                matrix.append((i + 1) * (y + 1)).append(" ");
+                if (y == 3) {
+                    matrix.append(System.lineSeparator());
+                }
             }
         }
-        String matrix = Arrays.deepToString(array);
+        String newMatrix = matrix.toString();
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write("Hello world!".getBytes());
             out.write(System.lineSeparator().getBytes());
-            out.write(matrix.getBytes());
-        } catch (Exception e) {
+            out.write(newMatrix.getBytes());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
