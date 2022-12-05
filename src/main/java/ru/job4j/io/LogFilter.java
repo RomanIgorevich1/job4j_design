f83dbbcf9b27654dcb56293f5e8c61418908bc0e
votmo <votmoyapochta1@bk.ru>
2022-12-05 20:46:06 +0300

    BufferedReader. [#252489 #367715]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -8,15 +8,13 @@ public class LogFilter {
     public List<String> filter(String filter) {
         List<String> newList = new ArrayList<>();
         try (BufferedReader input = new BufferedReader(new FileReader(filter))) {
-            StringBuilder text = new StringBuilder();
             String newText;
             while ((newText = input.readLine()) != null) {
-                    text.append(newText).append(System.lineSeparator());
-            }
-            String[] array = text.toString().split(System.lineSeparator());
-            for (String str : array) {
-                if (str.contains(" " + 404 + " ")) {
-                    newList.add(str);
+                String[] array = newText.split(System.lineSeparator());
+                for (String str : array) {
+                    if (str.contains("404 ")) {
+                        newList.add(str);
+                    }
                 }
             }
         } catch (IOException exception) {
@@ -40,7 +38,7 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> list = logFilter.filter("log.txt");
-        System.out.println(list);
+        list.forEach(System.out::println);
         List<String> log = logFilter.filter("log.txt");
         save(log, "404.txt");
     }
