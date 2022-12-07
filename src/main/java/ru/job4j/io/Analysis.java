9ca700d698d6ab7ce173a16be219868987ea4d07
votmo <votmoyapochta1@bk.ru>
2022-12-07 20:01:50 +0300

    Анализ доступности сервера. [#859 #368527]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -10,21 +10,18 @@ public class Analysis {
             boolean result = true;
             while ((text = reader.readLine()) != null) {
                 String[] newText = text.split(" ");
-                for (String str : newText) {
-                    if ((str.contains("400") || str.contains("500")) && result) {
-                        result = false;
-                        writer.printf("%s%n", newText[1]);
-                    }
-                    if ((str.contains("200") || str.contains("300")) && !result) {
-                        result = true;
-                        writer.printf("%s%n", newText[1]);
-                    }
+                if ((newText[0].equals("400") || newText[0].contains("500")) && result) {
+                    result = false;
+                    writer.printf("%s%n", newText[1]);
+                }
+                if ((newText[0].contains("200") || newText[0].contains("300")) && !result) {
+                    result = true;
+                    writer.printf("%s%n", newText[1]);
                 }
             }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
