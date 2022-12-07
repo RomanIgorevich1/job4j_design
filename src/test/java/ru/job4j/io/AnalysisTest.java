ae711000a96c367511052843a864e179dff54447
votmo <votmoyapochta1@bk.ru>
2022-12-07 21:15:22 +0300

    Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import java.io.*;
+import java.nio.file.Path;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class AnalysisTest {
+    @Test
+    void analysis(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("serverAnalysis.txt").toFile();
+        try (PrintWriter writer = new PrintWriter(source)) {
+            writer.println("200 10:56:01");
+            writer.println("500 10:57:01");
+            writer.println("400 10:58:01");
+            writer.println("300 10:59:01");
+            writer.println("500 11:01:02");
+            writer.println("200 11:02:02");
+        }
+        File target = tempDir.resolve("resultAnalysis.txt").toFile();
+        new Analysis().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().map(value -> value + " ").forEach(result::append);
+        }
+        assertThat("10:57:01 10:59:01 11:01:02 11:02:02 ").isEqualTo(result.toString());
+    }
+}
