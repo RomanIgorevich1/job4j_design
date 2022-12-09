cc19d6ef78397a0f674e2879e9f8647000602723
votmo <votmoyapochta1@bk.ru>
2022-12-09 12:18:02 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
@@ -7,13 +8,21 @@ import java.util.function.Predicate;
 
 public class Search extends SimpleFileVisitor<Path> {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".properties")).forEach(System.out::println);
+        Search search = new Search();
+        search.validation(args);
+        Path start = Path.of(args[0]);
+        search(start, p -> p.toFile().getName().contains(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searchFiles = new SearchFiles(condition);
         Files.walkFileTree(root, searchFiles);
         return searchFiles.getPaths();
+    }
+
+    public void validation(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root is null. Usage ROOT_FOLDER.");
+        }
     }
 }
