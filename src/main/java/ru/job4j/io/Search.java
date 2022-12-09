fc525190336b35b4892ebcc63a563b5be53225f9
votmo <votmoyapochta1@bk.ru>
2022-12-09 20:11:36 +0300

    Валидация параметров запуска. [#246865 #369279]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -20,8 +20,8 @@ public class Search extends SimpleFileVisitor<Path> {
     }
 
     public void validation(String[] args) {
-        if ((args.length == 0) || (args[0] == null || args[1] == null)) {
-            throw new IllegalArgumentException("Root is null. Usage ROOT_FOLDER. Or args == null.");
+        if (args.length != 2  || (!args[0].equals("C:") || !args[1].equals(".properties"))) {
+            throw new IllegalArgumentException("Root is null. Usage ROOT_FOLDER. Or wrong argument");
         }
     }
 }
