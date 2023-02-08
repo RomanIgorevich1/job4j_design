9ae2fefc611dddf5cec2c8f3473bd67b310f95b8
votmo <votmoyapochta1@bk.ru>
2023-02-08 14:06:37 +0300

    Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +11,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -26,6 +31,9 @@ public class EchoServer {
                     outputStream.flush();
                 }
             }
+        } catch (IOException exception) {
+            LOGGER.error("I/O error occurs when opening the socket. ", exception);
         }
     }
 }
+
