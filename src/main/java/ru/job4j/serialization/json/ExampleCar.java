782fc19482f8588914c1dab8c7905e03cc728acd
votmo <votmoyapochta1@bk.ru>
2023-02-13 20:52:45 +0300

    Формат JSON [#313164 #389864]

--- src/main/java/ru/job4j/serialization/json/ExampleCar.java
+++ src/main/java/ru/job4j/serialization/json/ExampleCar.java
@@ -1,25 +1,22 @@
 package ru.job4j.serialization.json;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 public class ExampleCar {
     public static void main(String[] args) {
-        final CreatingCar myCar = new CreatingCar(true, 2019,
-                new Car("Mazda"), new String[] {"Car Model", "Color", "Engine"});
+        final MyCar myCar = new MyCar(true, 2019, new ModelCar("Mazda 6"), new String[] {"Color", "Engine"});
        final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(myCar));
         final String myCarJson = "{"
-                + "\"type\":true,"
+                + "\"move\":true,"
                 + "\"year\":2019,"
-                + "\"car\":"
+                + "\"modelCar\":"
                 + "{"
-                + "\"brand\":\"Mazda\""
+                + "\"model\":\"Mazda\""
                 + "},"
                 + "\"descriptions\":"
                 + "[\"CX-5\",\"Black\", \"2.5\"]"
                 + "}";
-        final CreatingCar carMod = gson.fromJson(myCarJson, CreatingCar.class);
+        final MyCar carMod = gson.fromJson(myCarJson, MyCar.class);
         System.out.println(carMod);
     }
 }
