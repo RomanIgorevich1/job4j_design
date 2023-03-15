97467b694c7cd478c2c38e268ff9e63e544ca1a6
votmo <votmoyapochta1@bk.ru>
2023-03-15 06:01:44 +0300

    2. Триггеры [#504803]

---
+++ db/trigger.sql
@@ -0,0 +1,61 @@
+create table products (
+	id serial primary key,
+	name varchar(50),
+	producer varchar(50),
+	count integer default 0,
+	price integer
+);
+
+create table history_of_price (
+	id serial primary key,
+	name varchar(50),
+	price integer,
+	date timestamp
+);
+
+create trigger trigger_after
+after insert on products
+referencing new table as inserted
+execute procedure tax_after();
+
+create or replace function tax_after()
+returns trigger as 
+$$
+begin
+update products
+set price = price + price * 0.13
+where id = (select id from inserted) AND count >= 5;
+return new;
+end;
+$$
+language 'plpgsql';
+
+create or replace function tax_before()
+returns trigger as
+$$
+begin
+new.price = new.price + new.price * 0.13
+return new;
+end;
+$$
+language 'plpgsql';
+
+create trigger trigger_before
+before insert on products
+for each row
+execute procedure tax_before();
+
+create or replace function history_price()
+return trigger as
+$$
+begin
+insert into history_of_price(name, price, date) values (new.name, new.price, now());
+return new;
+end;
+$$
+language 'plpgsql';
+
+create trigger trigger_price
+after insert on products
+for each row
+execute procedure history_price();
