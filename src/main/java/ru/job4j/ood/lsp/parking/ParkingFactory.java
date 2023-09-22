72d04a6abede0b970c9f925755de2504827dd96b
votmo <votmoyapochta1@bk.ru>
2023-09-22 16:20:38 +0300

    Парковка машин [#853 #436816]

--- src/main/java/ru/job4j/ood/lsp/parking/ParkingFactory.java
+++ src/main/java/ru/job4j/ood/lsp/parking/ParkingFactory.java
@@ -10,5 +10,4 @@ public interface ParkingFactory {
     int getPlaceForCar();
 
     int getParkingSize();
-
 }
--- src/test/java/ru/job4j/ood/lsp/parking/ParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/ParkingTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.ood.lsp.parking;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
